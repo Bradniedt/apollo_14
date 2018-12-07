50e1ff9a955646bfd7f6a55411f5043151650e32
Brad Niedt <bradleyniedt@gmail.com>
2018-12-07 10:56:21 -0700

    final commit

--- spec/features/user_sees_all_missions_in_order_per_astro_spec.rb
+++ spec/features/user_sees_all_missions_in_order_per_astro_spec.rb
@@ -8,7 +8,7 @@ describe "as a user" do
       mission_1 = astro_1.missions.create(title: "Mars One", time_in_space: 500)
       mission_2 = astro_1.missions.create(title: "Hubble Reconstruction", time_in_space: 100)
       mission_3 = astro_1.missions.create(title: "Atlas Tests", time_in_space: 200)
-
+      
       expect(all('.missions')[0]).to have_content(mission_1.title)
       expect(all('.missions')[1]).to have_content(mission_2.title)
       expect(all('.missions')[2]).to have_content(mission_3.title)
