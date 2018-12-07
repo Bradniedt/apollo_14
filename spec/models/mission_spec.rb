edd650988b07ea0e51033a42a81ca577889b4a48
Brad Niedt <bradleyniedt@gmail.com>
2018-12-07 10:48:43 -0700

    adds test for mission model alphabetical

--- spec/models/mission_spec.rb
+++ spec/models/mission_spec.rb
@@ -10,4 +10,11 @@ describe Mission, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :astronauts}
   end
+
+  describe "class methods" do
+    mission_1 = astro_1.missions.create(title: "Mars One", time_in_space: 500)
+    mission_2 = astro_1.missions.create(title: "Hubble Reconstruction", time_in_space: 100)
+
+    expect(Mission.alphabetical).to eq([mission_2, mission_1])
+  end
 end
